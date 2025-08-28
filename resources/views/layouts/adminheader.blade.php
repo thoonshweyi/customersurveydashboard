2dfd9ea9f9bb9846c562188fba04a902fda1dc7d
Thoon She Yi <thoonlay779@gmail.com>
2025-08-28 10:56:57 +0630

    viewport

--- resources/views/layouts/adminheader.blade.php
+++ resources/views/layouts/adminheader.blade.php
@@ -5,6 +5,7 @@
         <title>{{ config('app.name') }}</title>
 
         <meta charseet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}"/>
 
         <!-- fav icon -->
